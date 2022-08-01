b81df4f5a25836e876eff838e6e36b5f6d4d3a40
Muralidhara Bhat K S <ksmuralidhara0@gmail.com>
2022-08-01 18:58:23 +0530

    New Version

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//---------- Navbar ----------//
+
+window.addEventListener("scroll", function () {
+	let navbar = document.getElementById("navbar");
+	navbar.classList.toggle("fixed", this.window.scrollY > 0);
+});
+
+// Portfolio Gallery
+
+// Review Carousel
