8b992e37cc1f3a8e2132cba0f377fb87d7b9d320
Muralidhara Bhat K S <ksmuralidhara0@gmail.com>
2022-08-04 21:59:29 +0530

    -Resume, +Skills, working on PF, +typing style

--- js/script.js
+++ js/script.js
@@ -5,6 +5,22 @@ window.addEventListener("scroll", function () {
 	navbar.classList.toggle("fixed", this.window.scrollY > 0);
 });
 
+// Typing animation
+var typed = new Typed(".typing", {
+	strings: ["Web Developer", "Software Developer", "UX Designer", "Freelancer"],
+	typeSpeed: 100,
+	backSpeed: 60,
+	backDelay: 1000,
+	loop: true,
+});
+
+var typed = new Typed(".type", {
+	strings: ["Web Developer", "Software Developer", "UX Designer", "Freelancer"],
+	typeSpeed: 100,
+	backSpeed: 60,
+	backDelay: 1000,
+	loop: true,
+});
 // Portfolio Gallery
 
 // Review Carousel
